115d7ed2410f6f4e6ea5e70e55295c8905b17571
Thiago Cruz <thiago2096@live.com>
2021-01-30 01:35:59 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'main.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js',
   },
   module: {
